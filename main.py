10436abe3727d49c8f0ee33ad0d341065c7c1e87
Koncar Alan (XC-AS/ENG-AU) <alan.koncar@au.bosch.com>
2025-10-08 10:19:57 +1100

    implemented circle implemented player based on circle updated main loop to draw player

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 =  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(FPS) / MS_TO_S
+        
 if __name__ == "__main__":
     main()
