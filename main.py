1124492b4b6d77e6d8d7881c8906666ec886936e
Koncar Alan (XC-AS/ENG-AU) <alan.koncar@au.bosch.com>
2025-10-08 15:55:23 +1100

    Implemented - player movements - created drawable and updatable containers

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ from player import Player
 def main():
     print("Starting Asteroids!")   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player_1 =  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     clock = pygame.time.Clock()
     dt = 0
+    
     while True:
+        # Allow game to exit gracefully
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Draw the background and all foreground object
         screen.fill("black")
-        player_1.draw(screen)
-        pygame.display.flip()
+        for object in drawable:
+            object.draw(screen)
+        
+        # Move the game clock and update all updatable objects 
         dt = clock.tick(FPS) / MS_TO_S
+        updatable.update(dt)
+        
+        # Render the screen
+        pygame.display.flip()
         
 if __name__ == "__main__":
     main()
